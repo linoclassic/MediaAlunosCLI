3519949fe94741bb982888a060cae292d23934b4
Victor Lino <linoxvictorgmail.com>
2023-07-17 13:18:25 -0300

    Add project files.

---
+++ CursoCSharp/Aluno.cs
@@ -0,0 +1,51 @@
+﻿namespace CursoCSharp
+{
+    public class Aluno
+    {
+        private double _notaFinal;
+        public string NomeAluno { get; set; }
+        public string Situacao { get; private set; }
+        public double Nota1 { get; private set; }
+        public double Nota2 { get; private set; }
+        public double Nota3 { get; private set; }
+        public double Nota4 { get; private set; }
+        public double MediaFinal { get; private set; }
+
+        public Aluno(string nome, double nota1, double nota2, double nota3, double nota4)
+        {
+            NomeAluno = nome;
+            Nota1 = nota1;
+            Nota2 = nota2;
+            Nota3 = nota3;
+            Nota4 = nota4;
+
+            MediaFinal = NotaFinal();
+            Situacao = VerSituacao();
+        }
+
+        private double NotaFinal()
+        {
+            double soma = (Nota1 + Nota2 + Nota3 + Nota4) / 4;
+            _notaFinal = soma;
+            return _notaFinal;
+        }
+
+        private string VerSituacao()
+        {
+            string situacao;
+
+            if (_notaFinal >= 6)
+            {
+                situacao = "Aprovado";
+            }
+            else
+            {
+                situacao = "Reprovado";
+            }
+            return situacao;
+        }
+
+    }
+
+
+}
---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Quantos alunos gostaria de calcular a média final? ");
+            int n = int.Parse(Console.ReadLine());
+            if (n > 0)
+            {
+                Aluno[] vect = new Aluno[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Nome do Aluno: ");
+                    string nome = Console.ReadLine();
+                    Console.Write("Nota 1: ");
+                    double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Nota 2: ");
+                    double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Nota 3: ");
+                    double n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Nota 4: ");
+                    double n4 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Aluno pessoa = new Aluno(nome, n1, n2, n3, n4);
+                    vect[i] = pessoa;
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine($"Nome: {vect[i].NomeAluno}, Media final: {vect[i].MediaFinal.ToString("F2", CultureInfo.InvariantCulture)}, Situação: {vect[i].Situacao}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Operação Inválida, por favor tente novamente!!!");
+            }
+        }
+    }
+}
